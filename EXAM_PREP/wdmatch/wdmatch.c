e3cd72e3fc2d8c955cb1919df574a698ee73e47a
Daram Bae <dabae@c2r4p3.42perpignan.fr>
2023-09-06 19:22:56 +0200

    completed wdmatch

--- EXAM_PREP/wdmatch/wdmatch.c
+++ EXAM_PREP/wdmatch/wdmatch.c
@@ -6,7 +6,7 @@
 /*   By: dabae <dabae@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 18:24:49 by dabae             #+#    #+#             */
-/*   Updated: 2023/09/06 19:13:05 by dabae            ###   ########.fr       */
+/*   Updated: 2023/09/06 19:22:31 by dabae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -25,22 +25,13 @@ int	main(int argc, char **argv)
         	        len++;
 		i = 0;
 		j = 0;
-		while (argv[1][i] != '\0' && argv[2][j] != '\0')
+		while (i < len && *argv[2])
 		{	
-			if (argv[1][i] == argv[2][j])
+			if (argv[1][i] == *argv[2]++)
 				i++;
-			else
-				j++;
 		}
-		a = 0;
-		if (i == len + 1)
-		{
-			while (a < len)
-			{
-				write(1, &argv[1][a], 1);
-				a++;
-			}
-		}
+		if (i == len)
+			write(1, argv[1], len);
 	}
 	write(1, "\n", 1);
 	return (0);
