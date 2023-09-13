c7493ea988e6faef561f45d7688338313d6cf29b
Daram Bae <dabae@c2r4p3.42perpignan.fr>
2023-09-13 17:07:39 +0200

    prep

--- EXAM_PREP/SECOND_TRY/inter.c
+++ EXAM_PREP/SECOND_TRY/inter.c
@@ -1,39 +1,58 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   wdmatch.c                                          :+:      :+:    :+:   */
+/*   inter.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dabae <dabae@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/13 15:27:45 by dabae             #+#    #+#             */
-/*   Updated: 2023/09/13 15:48:21 by dabae            ###   ########.fr       */
+/*   Created: 2023/09/13 15:56:31 by dabae             #+#    #+#             */
+/*   Updated: 2023/09/13 17:07:23 by dabae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
+int	self_check(char *s, char c, int	pos)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && i < pos)
+	{
+		if (s[i] == c)
+			return (1);
+	}
+	return (0);
+}
+
+int	check_other(char *s, char c)
+{
+	int	i;
+	
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (1);
+	}
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	int	i;
-	int	j;
-	int	z;
 
-	i = 0;
-	j = 0;
 	if (ac == 3)
 	{
-		while (av[1][i] && av[2][j])
+		i = 0;
+		while (av[1][i])
 		{
-			if (av[1][i] == av[2][j++])
-				i++;
-		}
-		if (av[1][i] == '\0')
-		{
-			z = 0;
-			while (av[1][z])
+	       		if (!self_check(av[1], av[1][i], i) && check_other(av[2], av[1][i]))
 			{
-				write(1, &av[1][z], 1);
-				z++;
+				write(1, &av[1][i], 1);
+				i++;
 			}
+			else
+				break;
 		}
 	}
 	write(1, "\n", 1);
